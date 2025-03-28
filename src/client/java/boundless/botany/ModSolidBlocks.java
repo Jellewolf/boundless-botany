57e0d01eb32382d44bb27f161def231f18de4230
Jelle <63313141+Jellewolf@users.noreply.github.com>
2025-03-28 11:35:07 +0100

    update: expended blacklist of blocks

--- src/client/java/boundless/botany/ModSolidBlocks.java
+++ src/client/java/boundless/botany/ModSolidBlocks.java
@@ -2,8 +2,7 @@ package boundless.botany;
 
 import java.util.HashSet;
 import java.util.Set;
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
 
@@ -27,11 +26,65 @@ public class ModSolidBlocks {
     for (Identifier id : Registries.BLOCK.getIds()) {
       Block block = Registries.BLOCK.get(id);
 
-      if (block.getDefaultState().isOpaque()) {
-        if (!VANILLA_PLANTABLE_BLOCKS.contains(block)) {
-          CONFIGURABLE_PLANTABLE_BLOCKS.add(block);
-          BLOCK_NAMES.add(id.toString());
-        }
+      if (block.getDefaultState().isOpaque()
+          && !(block instanceof SlabBlock)
+          && !(block instanceof StairsBlock)
+          && !(block instanceof WallBlock)
+          && !(block instanceof FenceBlock)
+          && !(block instanceof FenceGateBlock)
+          && !(block instanceof TrapdoorBlock)
+          && !(block instanceof DoorBlock)
+          && !(block instanceof BannerBlock)
+          && !(block instanceof PaneBlock)
+          && !(block instanceof LadderBlock)
+          && !(block instanceof ChainBlock)
+          && !(block instanceof ScaffoldingBlock)
+          && !(block instanceof LightningRodBlock)
+          && !(block instanceof EndRodBlock)
+          && !(block instanceof LecternBlock)
+          && !(block instanceof EnchantingTableBlock)
+          && !(block instanceof BellBlock)
+          && !(block instanceof ChestBlock)
+          && !(block instanceof BarrelBlock)
+          && !(block instanceof BeaconBlock)
+          && !(block instanceof CraftingTableBlock)
+          && !(block instanceof SmithingTableBlock)
+          && !(block instanceof ShulkerBoxBlock)
+          && !(block instanceof SkullBlock)
+          && !(block instanceof WallSkullBlock)
+          && !(block instanceof SignBlock)
+          && !(block instanceof WallSignBlock)
+          && !(block instanceof HangingSignBlock)
+          && !(block instanceof WallHangingSignBlock)
+          && !(block instanceof CakeBlock)
+          && !(block instanceof CandleBlock)
+          && !(block instanceof CandleCakeBlock)
+          && !(block instanceof BigDripleafBlock)
+          && !(block instanceof SmallDripleafBlock)
+          && !(block instanceof SporeBlossomBlock)
+          && !(block instanceof MushroomBlock)
+          && !(block instanceof LilyPadBlock)
+          && !(block instanceof CarpetBlock)
+          && !(block instanceof EndPortalFrameBlock)
+          && !(block instanceof EndPortalBlock)
+          && !(block instanceof NetherPortalBlock)
+          && !(block instanceof BedBlock)
+          && !(block instanceof RespawnAnchorBlock)
+          && !(block instanceof CauldronBlock)
+          && !(block instanceof RedstoneTorchBlock)
+          && !(block instanceof RedstoneWireBlock)
+          && !(block instanceof LeverBlock)
+          && !(block instanceof PressurePlateBlock)
+          && !(block instanceof DaylightDetectorBlock)
+          && !(block instanceof PistonBlock)
+          && !(block instanceof ObserverBlock)
+          && !(block instanceof DispenserBlock)
+          && !(block instanceof DropperBlock)
+          && !(block instanceof HopperBlock)
+          && !VANILLA_PLANTABLE_BLOCKS.contains(block)) {
+
+        CONFIGURABLE_PLANTABLE_BLOCKS.add(block);
+        BLOCK_NAMES.add(id.toString());
       }
     }
   }
